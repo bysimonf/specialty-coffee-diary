3ecc3c0bd97205ef9894a39a54f301c2079e36fa
Simon Frohnauer <simon.frohnauer@gmx.de>
2023-03-09 17:23:37 +0100

    storing my progress

--- app/javascript/controllers/bookmark_coffee_controller.js
+++ app/javascript/controllers/bookmark_coffee_controller.js
@@ -4,4 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
   }
+
+  bookmarkCoffee(event) {
+    console.log(event)
+    // call bookmark method create
+    // set bookmark status to true in the coffee model
+    // change icon on a coffee (ternary operator)
+  }
 }
