bb9f383e58ac15dcf1035fe06099e97f0b47075c
Simon Frohnauer <simon.frohnauer@gmx.de>
2023-03-14 11:39:41 +0100

    building about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "/about", to: "application#about"
+
+  # Define you application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :coffees, only: [:index, :show, :create, :new] do
     resources :bookmarks, only: [:create]
     resources :reviews, only: [:index, :new, :create]
