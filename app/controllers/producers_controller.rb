47bf09d2e3bfc7996dbcdce0e94a84e902c13cb1
bethelhem abegaz <bethelhemmesfin121@gmail.com>
2023-03-14 12:43:57 +0100

    add link of producer to coffee

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -6,8 +6,22 @@ class ProducersController < ApplicationController
       {
         lat: producer.latitude,
         lng: producer.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {producer: producer})
+        info_window_html: render_to_string(partial: "info_window", locals: { producer: producer })
       }
     end
+  end
+
+  def show
+    @coffee = Coffee.find(params[:id])
+    @producer = @coffee.producer
+    @markers = [{
+      lng: @producer.longitude,
+      lat: @producer.latitude,
+      info_window_html: render_to_string(partial: "info_window", locals: { coffee: @coffee, producer: @producer })
+      }]
+  end
+
+  def coffee_params
+    params.require(:coffee).permit(:origin, :description, :producer_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :coffees, only: [:index, :show, :create, :new] do
     resources :bookmarks, only: [:create]
+    resources :producers, only: [:show]
     resources :reviews, only: [:index, :new, :create]
   end
 
